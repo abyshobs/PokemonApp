5d962838c14204c6227f3d03dd904c57fc42633a
abiola shobande <abyshobs@hotmail.co.uk>
2022-11-21 18:59:56 +0000

    more code cleanup, externalised some of the service registerations

---
+++ src/PokemonApi/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using PokemonApi.Repository;
+using PokemonApi.Repository.Factories;
+using PokemonApi.Repository.Interfaces;
+using PokemonApi.Services;
+using PokemonApi.Services.Interfaces;
+
+namespace PokemonApi.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static void ConfigureHttpClientFactories(this WebApplicationBuilder builder)
+        {
+
+            builder.Services.AddHttpClient("PokeApi", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(builder.Configuration["APIs:PokemonAPI"]);
+            });
+
+            builder.Services.AddHttpClient("FunTranslationsApi", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(builder.Configuration["APIs:FunTranslationsAPI"]);
+            });
+        }
+
+        public static void ConfigureRepositories(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<IPokemonRepository, PokemonRepository>();
+            builder.Services.AddTransient<ITranslationRespository, YodaTranslator>();
+            builder.Services.AddTransient<ITranslationRespository, ShakespeareTranslator>();
+        }
+
+        public static void ConfigureApplicationServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<IPokemonService, PokemonService>();
+            builder.Services.AddTransient<ITranslationService, TranslationService>();
+        }
+    }
+}
--- src/PokemonApi/Program.cs
+++ src/PokemonApi/Program.cs
@@ -1,38 +1,18 @@
-using PokemonApi.Repository;
-using PokemonApi.Repository.Factories;
-using PokemonApi.Repository.Interfaces;
-using PokemonApi.Services;
-using PokemonApi.Services.Interfaces;
+using PokemonApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-
-builder.Services.AddHttpClient("PokeApi", httpClient =>
-{
-    httpClient.BaseAddress = new Uri(builder.Configuration["APIs:PokemonAPI"]);
-});
-
-builder.Services.AddHttpClient("FunTranslationsApi", httpClient =>
-{
-    httpClient.BaseAddress = new Uri(builder.Configuration["APIs:FunTranslationsAPI"]);
-});
-
-builder.Services.AddTransient<IPokemonRepository, PokemonRepository>();
-builder.Services.AddTransient<IPokemonService, PokemonService>();
-builder.Services.AddTransient<ITranslationService, TranslationService>();
-builder.Services.AddTransient<ITranslationRespository, YodaTranslator>();
-builder.Services.AddTransient<ITranslationRespository, ShakespeareTranslator>();
+builder.ConfigureHttpClientFactories();
+builder.ConfigureRepositories();
+builder.ConfigureApplicationServices();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
